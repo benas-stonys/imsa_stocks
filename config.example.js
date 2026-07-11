f72ca61f04584e0652a02c6fe613a713cdb05aca
benas <ben.stonys@gmail.com>
2026-07-10 20:09:53 -0600

    removed the example

--- config.example.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'jpuquodeajpqsvibvlss.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_uu6Js1wOYDTU3xoyzGpgnw_nIFMMSEw';
-export const API_BASE = '/.netlify/functions';
