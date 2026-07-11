ac2b8047f327ccb1af79d258d1e367825e76b75e
benas <ben.stonys@gmail.com>
2026-07-10 19:48:20 -0500

    including config.js since it can be public

---
+++ config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://jpuquodeajpqsvibvlss.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_uu6Js1wOYDTU3xoyzGpgnw_nIFMMSEw';
+export const API_BASE = '/.netlify/functions';
